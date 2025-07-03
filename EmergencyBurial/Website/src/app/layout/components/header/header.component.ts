cfcb55cd261bb76f702b2461f7ee9d622b63d285
OzS <ozs@dat.gov.il>
2025-07-03 13:02:47 +0300

    Create header

--- EmergencyBurial/Website/src/app/layout/components/header/header.component.ts
+++ EmergencyBurial/Website/src/app/layout/components/header/header.component.ts
@@ -1,36 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthContextService } from 'src/app/shared/services/auth-context.service';
-import {IMember} from '../../../shared/model/member';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthContextService} from 'src/app/shared/services/auth-context.service';
+import {NgOptimizedImage} from '@angular/common';
+import {MatTab, MatTabGroup} from '@angular/material/tabs';
 import {constants} from '../../../shared/static/constants';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatFormFieldControl} from '@angular/material/form-field';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
 
-@Component( {
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss'],
-    standalone: true,
-    imports: [],
-} )
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [
+    NgOptimizedImage,
+    MatTabGroup,
+    MatTab,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatIcon,
+    MatIconButton,
+    FormsModule,
+  ],
+})
 export class HeaderComponent implements OnInit {
 
-  isOpen: boolean = true;
-  public member: IMember = {};
+  public searchValue: string;
 
-  constructor( public authCtx: AuthContextService,
-               private router: Router
-  ) {}
-
-  async ngOnInit() {
+  constructor(public authCtx: AuthContextService,
+              private router: Router,
+  ){
   }
 
-  onToggleMenu() {
-    this.isOpen = !this.isOpen;
+  async ngOnInit(){
   }
 
-  signOut() {
+  signOut(){
     this.authCtx.Token = null;
-    this.router.navigate( [ 'sessions/login' ] );
+    this.router.navigate(['sessions/login']);
   }
 
   protected readonly constants = constants;
+
+  applyFilter(value: any){
+
+  }
+
+  clearSearchField(){
+    this.searchValue = null;
+  }
 }
--- EmergencyBurial/Website/src/app/shared/static/constants.ts
+++ EmergencyBurial/Website/src/app/shared/static/constants.ts
@@ -2,4 +2,5 @@ export let constants = {
   minYearList: 2022,
   emptyPdfFile: './assets/empty.pdf',
   employmentDiversityPdf: './assets/employmentDiversity.pdf',
+  israelLogo: './assets/images/Israel_Logo.png',
 };
