1d62678a9920a7f4498c4e7f318f2716496d6c84
OzS <ozs@dat.gov.il>
2025-07-20 14:18:47 +0300

    Create deceased dialog html

--- EmergencyBurial/Website/src/app/views/components/deceased/components/deceased-dialog/deceased-dialog.component.ts
+++ EmergencyBurial/Website/src/app/views/components/deceased/components/deceased-dialog/deceased-dialog.component.ts
@@ -10,10 +10,17 @@ import {IColumn} from "../../../../../shared/ui-components/model/column";
 })
 export class DeceasedDialogComponent implements OnInit {
 
-  @Input() deceased:Deceased;
+  @Input() deceased: Deceased;
   @Input() cols: IColumn[] = [];
 
-  ngOnInit(){
+  colsPart1: IColumn[] = [];
+  colsPart2: IColumn[] = [];
+
+  ngOnInit() {
+
+    const splitIndex = 4; // שנה את זה למספר העמודות הרצוי בחלק הראשון
+    this.colsPart1 = this.cols.slice(0, splitIndex);
+    this.colsPart2 = this.cols.slice(splitIndex);
 
   }
 
