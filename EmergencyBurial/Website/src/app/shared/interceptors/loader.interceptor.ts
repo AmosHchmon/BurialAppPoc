94abfe5fc149195e349fdb450361800b89742e5f
OzS <ozs@dat.gov.il>
2025-08-03 13:02:59 +0300

    Implement primeNG toast

--- EmergencyBurial/Website/src/app/shared/interceptors/loader.interceptor.ts
+++ EmergencyBurial/Website/src/app/shared/interceptors/loader.interceptor.ts
@@ -1,6 +1,7 @@
 ﻿import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { finalize } from 'rxjs/operators';
+
 import { LoaderService } from '../services/loader.service';
 
 export const loaderInterceptor: HttpInterceptorFn = (req, next) => {
