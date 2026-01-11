7513945738a31aebb24334a58e4948a7ed0a2cd2
OzS <ozs@dat.gov.il>
2026-01-11 15:16:04 +0200

    Fix conflicts

--- EmergencyBurial/Website/src/app/app.routes.ts
+++ EmergencyBurial/Website/src/app/app.routes.ts
@@ -42,7 +42,7 @@ export const routes: Routes = [
       },
       {
         path: 'deceaseds',
-        canMatch: [RoleGuard], data: {roles: [enmOrganizationType.Tarah, enmOrganizationType.DatServices]},
+        canMatch: [RoleGuard], data: {roles: [enmOrganizationType.Tarah, enmOrganizationType.DatServices, enmOrganizationType.Hamal]},
         loadChildren: () => import('./features/deceased/deceased.routing').then(m => m.DeceasedRouting)
       },
       {
