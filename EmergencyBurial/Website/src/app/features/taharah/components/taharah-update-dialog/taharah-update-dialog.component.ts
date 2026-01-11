a09b8432ac193cdac9ac2167b2f155e1c4c83fd2
OzS <ozs@dat.gov.il>
2026-01-11 15:26:27 +0200

    Put EmptyDataComponent in UIComponents module

--- EmergencyBurial/Website/src/app/features/taharah/components/taharah-update-dialog/taharah-update-dialog.component.ts
+++ EmergencyBurial/Website/src/app/features/taharah/components/taharah-update-dialog/taharah-update-dialog.component.ts
@@ -7,12 +7,11 @@ import {AlertService} from "../../../../shared/services/alert.service";
 import {TaharahProcess} from "../../model/TaharahProcess";
 import {AlertType} from "../../../../core/enums/alert.enum";
 import {DialogMessage} from "../../../../shared/static/messages";
-import {EmptyDataComponent} from "../../../../shared/components/empty-data/empty-data.component";
 
 @Component({
   selector: 'app-taharah-update-dialog',
   standalone: true,
-  imports: [UiComponentsModule, EmptyDataComponent],
+  imports: [UiComponentsModule],
   templateUrl: './taharah-update-dialog.component.html'
 })
 export class TaharahUpdateDialogComponent implements OnChanges {
